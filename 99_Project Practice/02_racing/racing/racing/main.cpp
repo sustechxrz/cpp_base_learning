139c866bc152798a3402f49b8ce7337159035f5a
sustechxrz <xrzstudyhard@163.com>
2025-10-11 12:49:42 +0800

    2025.10.11.轨道绘制

--- 99_Project Practice/02_racing/racing/racing/main.cpp
+++ 99_Project Practice/02_racing/racing/racing/main.cpp
@@ -1,13 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 using namespace sf;
+using namespace std;
 
 const int WinWidth = 1024;
 const int WinHeight = 768;
+const int roadWidth = 1800;
+const int roadSegLength = 180;
+const int roadCount = 2000;
+
+struct Road {
+    float x, y, z;
+    float X, Y, W;
+    float scale;
+    Road(int _x, int _y, int _z) : x(_x), y(_y), z(_z) {}
+    void project(int camX, int camY, int camZ) {
+        scale = 1.0f / (z - camZ);
+        X = (1 + (x - camX) * scale) * WinWidth / 2;
+        Y = (1 - (y - camY) * scale) * WinHeight / 2;
+        W = scale * roadWidth * WinWidth / 2;
+    }
+
+};
+
+void DrawTrape(RenderWindow& window, Color c, int x1, int y1, int w1, int x2, int y2, int w2) {
+    ConvexShape polygon(4);
+    polygon.setFillColor(c);
+    polygon.setPoint(0, Vector2f(x1-w1, y1));
+    polygon.setPoint(1, Vector2f(x2-w2, y2));
+    polygon.setPoint(2, Vector2f(x2+w2, y2));
+    polygon.setPoint(3, Vector2f(x1+w1, y1));
+    window.draw(polygon);
+}
 
 int main()
 {
     RenderWindow window(VideoMode(WinWidth, WinHeight), "Racing");
     window.setFramerateLimit(60);
+
+    vector <Road>roads;
+    for (int i = 0; i < roadCount; i++) {
+        Road r(0, 0, (i + 1) * roadSegLength);
+        roads.push_back(r);
+    }
+
     while (window.isOpen())
     {
         Event event;
@@ -16,6 +52,23 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
+        window.clear();
+        //DrawTrape(window, Color::White, WinWidth/2, 500, 200, WinWidth/2, 300, 100);
+        for (int i = 0; i < 300; ++i) {
+            Road& now = roads[i];
+            now.project(0, 1600, 0);
+            if (!i) {
+                continue;
+            }
+            Road& pre = roads[i - 1];
+            Color grass = (i / 3) % 2 ? Color(16, 210, 16) : Color(0, 199, 0);
+            Color edge = (i / 3) % 2 ? Color(0, 0, 0) : Color(255, 255, 255);
+            Color road = (i / 3) % 2 ? Color(105, 105, 105) : Color(101, 101, 101);
+            DrawTrape(window, grass, pre.X, pre.Y, WinWidth, now.X, now.Y, WinWidth);
+            DrawTrape(window, edge, pre.X, pre.Y, pre.W * 1.3, now.X, now.Y, now.W * 1.3);
+            DrawTrape(window, road, pre.X, pre.Y, pre.W, now.X, now.Y, now.W);
+        }
+        window.display();
     }
     return 0;
 }
